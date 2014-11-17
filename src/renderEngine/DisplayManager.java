c610f291b27e62131b7a00faeaa5b3516611db74
GSamuel <gideon_hoog@hotmail.com>
2014-11-17 18:23:40 +0100

    added right libraries

---
+++ src/renderEngine/DisplayManager.java
@@ -0,0 +1,11 @@
+package renderEngine;
+
+public class DisplayManager
+{
+	public static void createDisplay()
+	{
+		Display.setDisplayMode();
+	}
+	public static void updateDisplay();
+	public static void closeDisplay();
+}
